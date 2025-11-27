76fecf340fb505025d3b07f98ab189430f38d694
LaCall-Gagans-Studio <studio@lacall-gagans.com>
2025-11-27 19:25:40 +0900

    error fixed

--- my-app/src/app/(frontend)/layout.tsx
+++ my-app/src/app/(frontend)/layout.tsx
@@ -1,6 +1,7 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import { ReactNode } from 'react'
+import './styles.css'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
